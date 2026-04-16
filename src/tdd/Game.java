67b78b8c56c769d21e7ec9f204d073842af0de5d
Thomas-B1007 <thomas.bjn65@yahoo.com>
2026-04-16 18:58:27 +0200

    ITER3

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,12 +1,13 @@
 package tdd;
 
 public class Game {
+	private int score = 0;
 
 	public void roll(int nbQuilles) {
-		
+		score = score + nbQuilles;
 	}
 	
 	public int score() {
-		return 0;
+		return score;
 	}
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -25,4 +25,12 @@ class GameTest {
 		}
 		assertEquals(0, game.score());
 	}
+	
+	@Test
+	void iter3Test() {
+		for (int i = 0; i < 20; i++) {
+			game.roll(1);
+		}
+		assertEquals(20, game.score());
+	}
 }
