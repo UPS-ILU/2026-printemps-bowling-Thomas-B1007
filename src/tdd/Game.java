8d45248d62d5bf82627cb8daf887ec222bef2f20
Thomas-B1007 <thomas.bjn65@yahoo.com>
2026-04-16 19:58:22 +0200

    ITER7 v2

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -2,37 +2,82 @@ package tdd;
 
 public class Game {
 	private int score = 0;
-	private boolean debutTour = true;
 	private int scoreTour = 0;
 	private int nbBonusSpare = 0;
-	private int nbBonusStrike = 0;
-
-	public void roll(int nbQuilles) {
-		score = score + nbQuilles;
-		scoreTour = scoreTour + nbQuilles;
+	private int tour = 1;
+	private int[] tableauStrike = {0,0,0};
+	
+	private int calculerBonusSpare(int nbQuilles) {
 		if (nbBonusSpare > 0) {
-			score = score + nbQuilles;
 			nbBonusSpare--;
+			return nbQuilles;
 		}
-		if (nbBonusStrike > 0) {
-			score = score + nbQuilles;
-			nbBonusStrike--;
-		}
-		if (scoreTour == 10) {
-			if (debutTour) {
-				nbBonusStrike = nbBonusStrike + 2;
-				debutTour = false;
-			} else {
-				nbBonusSpare++;
-			}
-		}
-		if (!debutTour) {
-			scoreTour = 0;
-		}
-		debutTour = !debutTour;
+		return 0;
 	}
 	
+	private int calculerBonusStrike(int nbQuilles) {
+		int bonus = 0;
+		for (int i = 0; i < 3; i++) {
+			if (tableauStrike[i] > 0) {
+				bonus = bonus + nbQuilles;
+				tableauStrike[i]--;
+			}
+		}
+		return bonus;
+	}
+	
+	private void ajouterStrike() {
+		int i = 0;
+		while (tableauStrike[i] > 0) i++;
+		tableauStrike[i] = 2;
+	}
+	
+	public void roll(int nbQuilles) {
+		score = score + nbQuilles + calculerBonusStrike(nbQuilles) + calculerBonusSpare(nbQuilles);
+		scoreTour = scoreTour + nbQuilles;
+		if (tour%2 == 1) {
+			if (scoreTour == 10) {
+				ajouterStrike();
+				tour++;
+				scoreTour = 0;
+			}
+		} else {
+			if (scoreTour == 10) {
+				nbBonusSpare++;
+			}
+			scoreTour = 0;
+		}
+		tour++;
+		System.out.println("score : " + score);
+	}
+
+//	public void roll(int nbQuilles) {
+//		score = score + nbQuilles;
+//		scoreTour = scoreTour + nbQuilles;
+//		if (nbBonusSpare > 0) {
+//			score = score + nbQuilles;
+//			nbBonusSpare--;
+//		}
+//		if (nbBonusStrike > 0) {
+//			score = score + nbQuilles;
+//			nbBonusStrike--;
+//		}
+//		if (scoreTour == 10) {
+//			if (debutTour) {
+//				nbBonusStrike = nbBonusStrike + 2;
+//				debutTour = false;
+//			} else {
+//				nbBonusSpare++;
+//			}
+//		}
+//		if (!debutTour) {
+//			scoreTour = 0;
+//		}
+//		debutTour = !debutTour;
+//	}
+	
 	public int score() {
+		System.out.println("-------");
 		return score;
 	}
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -71,4 +71,13 @@ class GameTest {
 		}
 		assertEquals(122, game.score());
 	}
+	
+	@Test
+	void iter8Test() {
+		int[] tabRoll = {1, 2, 10, 0, 10, 4, 2, 0, 10, 6, 2, 0, 10, 6, 4, 8, 2, 2, 8, 8};
+		for (int i = 0; i < tabRoll.length; i++) {
+			game.roll(tabRoll[i]);
+		}
+		assertEquals(131, game.score());
+	}
 }
