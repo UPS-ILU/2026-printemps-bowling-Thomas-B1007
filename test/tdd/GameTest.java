09e78f292be6b9cb737178c26d9da70f2d52ff7f
Thomas-B1007 <thomas.bjn65@yahoo.com>
2026-04-16 18:49:23 +0200

    ITER1

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -2,18 +2,19 @@ package tdd;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GameTest {
-
-	@Test
-	void testRoll() {
-		fail("Not yet implemented");
+	Game game;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		game = new Game();
 	}
 
 	@Test
-	void testScore() {
-		fail("Not yet implemented");
+	void iter1Test() {
+		assertEquals(0, game.score());
 	}
-
 }
